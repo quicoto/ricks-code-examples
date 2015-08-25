504c86ae8d46b2cdd80014dfd8d6ed0e61c52ca2
Ricard Torres <ricard@Ricards-MacBook-Pro.local>
2015-08-25 20:56:23 +0200

    Earthview: so jQuery is required

--- earthview/index.php
+++ earthview/index.php
@@ -59,9 +59,12 @@
 		</div> <!-- .footer-copyright -->
 	</footer>
 
+	<!-- jQuery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
 	<!-- Compiled and minified JavaScript -->
 	<script src="assets/js/materialize.min.js"></script>
-	
+
 	<script src="assets/js/scripts.js"></script>
 </body>
 </html>
